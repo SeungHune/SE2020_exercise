2faa204e8b835dd2b225d5f76d0da3ae2b9cb119
SeungHuneBaek <xyz9080@naver.com>
2020-03-19 10:45:36 +0900

    print your introduce when you input 2.

--- week1_exercise.py
+++ week1_exercise.py
@@ -1,9 +1,11 @@
 while True:
-    a = int(input("1 or 2 plz (3 for exit) : "))
-    if a == 3:
+    a = input("1 or 2 plz (3 for exit) : ")
+    if a == "3":
         print("good bye!")
         break
-    elif a == 1:
+    elif a == "1":
         print("my name is 백승헌.")
+    elif a == "2":
+        print("your introduce your introduce")
     else :
         pass
