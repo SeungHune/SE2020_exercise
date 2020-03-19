ef8d39f532ad99deb0cd0d94cf79747f96502034
SeungHuneBaek <xyz9080@naver.com>
2020-03-19 10:32:48 +0900

    input 1 then print my name

---
+++ week1_exercise.py
@@ -0,0 +1,9 @@
+while True:
+    a = int(input("1 or 2 plz (3 for exit) : "))
+    if a == 3:
+        print("good bye!")
+        break
+    elif a == 1:
+        print("my name is 백승헌.")
+    else :
+        pass
